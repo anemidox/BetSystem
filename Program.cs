24a4ff4b7bf9bcb20041cf7bc84fc15fd73d1df7
anemidox <dhanujadisanayke@gmail.com>
2025-08-02 00:01:29 +0530

    fix(main) : type error fix devide to divide

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program {
+    static void Main() {
+        int num1 = 10;
+        int num2 = 5;
+
+        int add = num1 + num2;
+        int subtract = num1 - num2;
+        int multiply = num1 * num2;
+        int divide = num1 / num2;
+
+        Console.WriteLine("Add : " + add);
+        Console.WriteLine("Subtract : " + subtract);
+        Console.WriteLine("Multiply : " + multiply);
+        Console.WriteLine("Divide : " + divide);
+    }
+}
